5ba02e0431bdcb967386b23deeeac44e3c066ca1
will <will@yager.io>
2019-06-23 15:46:27 +0800

    ok

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,7 @@
 module Main where
 
-import qualified Lib
-import qualified Flags
+import Smap.Commands (run)
+import Smap.Flags (command)
 
 main :: IO ()
-main = do
-    command <- Flags.command
-    Lib.run command
+main = run =<< command
